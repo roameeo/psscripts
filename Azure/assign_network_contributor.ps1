8b644cfb26e00ca26c701c939b912e68493dcd32
Stormy Winters <111472862+roameeo@users.noreply.github.com>
2026-04-01 09:01:06 -0500

    removed tenant and subscription info

--- Azure/assign_network_contributor.ps1
+++ Azure/assign_network_contributor.ps1
@@ -1,15 +1,15 @@
 
 # Authenticate to Tenant B
-$RemoteTenantId = "ffcdba76-444a-4e44-8c97-81cfe8d14cf2"
-$RemoteSubId = "20874075-e0dc-4c82-b624-f442cf42c0d6"
+$RemoteTenantId = "<tenantID>"
+$RemoteSubId = "<subscriptionID>"
 
 Connect-AzAccount -Tenant $RemoteTenantId -Subscription $RemoteSubId
 Select-AzSubscription -SubscriptionId $RemoteSubId
 
 # Define parameters
-$ObjectId = "7e25685b-a55e-411a-8e07-068a281dc75b"
+$ObjectId = "<ObjectID>" # Object ID of the user or service principal in Tenant B
 $RoleName = "Network Contributor"
-$Scope = "/subscriptions/20874075-e0dc-4c82-b624-f442cf42c0d6/resourceGroups/AZ-DataWarehouse-01/providers/Microsoft.Network/virtualNetworks/AZ-DATABASE-VN"
+$Scope = "<Scope>" # e.g., "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}"
 
 # Assign the role
 New-AzRoleAssignment -ObjectId $ObjectId -RoleDefinitionName $RoleName -Scope $Scope
